37b22c584f2ffbb1de5d02466f23963030133894
lilikazine <lilikazine@gmail.com>
2018-02-26 15:58:34 +0800

    Initial Commit

---
+++ HNAAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HNAAnimation
+//
+//  Created by LiLi Kazine on 2018/2/26.
+//  Copyright © 2018年 Hainan eKing Technology Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
